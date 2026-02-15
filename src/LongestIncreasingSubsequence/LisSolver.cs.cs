545f99c0bca2475ba730d90db941d8e5ecaa824d
Shashikant Ranjan <s.kant23456789@gmail.com>
2026-02-15 11:20:34 +0530

    Added initial LIS solver skeleton implementation

---
+++ src/LongestIncreasingSubsequence/LisSolver.cs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LongestIncreasingSubsequence
+{
+    public class LisSolver
+    {
+        public string GetLongestIncreasingSubsequence(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
+
+            var numbers = input
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            if (numbers.Length == 0)
+                return string.Empty;
+
+            // Temporary simple logic:
+            // Return first number (we will improve later)
+            return numbers[0].ToString();
+        }
+    }
+}
--- src/LongestIncreasingSubsequence/Program.cs
+++ src/LongestIncreasingSubsequence/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LongestIncreasingSubsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers separated by single whitespace:");
+            string input = Console.ReadLine();
+
+            var solver = new LisSolver();
+            string result = solver.GetLongestIncreasingSubsequence(input);
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(result);
+        }
+    }
+}
